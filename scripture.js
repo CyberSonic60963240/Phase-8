102a77c261473e2361bfb982c73f86b6bbf3d06f
CyberSonic60963240 <Gregory_Cowgill@davincischools.org>
2019-03-06 14:30:15 -0800

    Add files via upload

---
+++ scripture.js
@@ -0,0 +1,169 @@
+function nextImage(el){
+	if (el.src.match("A2.png")){
+		el.src = "A3.png";
+	} else if (el.src.match("A3.png")){
+		el.src = "A1.png";
+	} else if (el.src.match("A1.png")){
+		el.src = "A2.png";
+	} else {}}
+
+function nextImage2(el){
+	if (el.src.match("E2.png")){
+		el.src = "E3.png";
+	} else if (el.src.match("E3.png")){
+		el.src = "E1.png";
+	} else if (el.src.match("E1.png")){
+		el.src = "E2.png";
+	} else {}}
+
+function nextImage3(el){
+	if (el.src.match("I2.png")){
+		el.src = "I3.png";
+	} else if (el.src.match("I3.png")){
+		el.src = "I1.png";
+	} else if (el.src.match("I1.png")){
+		el.src = "I2.png";
+	} else {}}
+
+function nextImage4(el){
+	if (el.src.match("M2.png")){
+		el.src = "M3.png";
+	} else if (el.src.match("M3.png")){
+		el.src = "M1.png";
+	} else if (el.src.match("M1.png")){
+		el.src = "M2.png";
+	} else {}}
+
+function nextImage5(el){
+	if (el.src.match("B2.png")){
+		el.src = "B3.png";
+	} else if (el.src.match("B3.png")){
+		el.src = "B1.png";
+	} else if (el.src.match("B1.png")){
+		el.src = "B2.png";
+	} else {}}
+
+function nextImage6(el){
+	if (el.src.match("F2.png")){
+		el.src = "F3.png";
+	} else if (el.src.match("F3.png")){
+		el.src = "F1.png";
+	} else if (el.src.match("F1.png")){
+		el.src = "F2.png";
+	} else {}}
+
+function nextImage7(el){
+	if (el.src.match("J2.png")){
+		el.src = "J3.png";
+	} else if (el.src.match("J3.png")){
+		el.src = "J1.png";
+	} else if (el.src.match("J1.png")){
+		el.src = "J2.png";
+	} else {}}
+
+function nextImage8(el){
+	if (el.src.match("N2.png")){
+		el.src = "N3.png";
+	} else if (el.src.match("N3.png")){
+		el.src = "N1.png";
+	} else if (el.src.match("N1.png")){
+		el.src = "N2.png";
+	} else {}}
+
+function nextImage9(el){
+	if (el.src.match("C2.png")){
+		el.src = "C3.png";
+	} else if (el.src.match("C3.png")){
+		el.src = "C1.png";
+	} else if (el.src.match("C1.png")){
+		el.src = "C2.png";
+	} else {}}
+
+function nextImage10(el){
+	if (el.src.match("G2.png")){
+		el.src = "G3.png";
+	} else if (el.src.match("G3.png")){
+		el.src = "G1.png";
+	} else if (el.src.match("G1.png")){
+		el.src = "G2.png";
+	} else {}}
+
+function nextImage11(el){
+	if (el.src.match("K2.png")){
+		el.src = "K3.png";
+	} else if (el.src.match("K3.png")){
+		el.src = "K1.png";
+	} else if (el.src.match("K1.png")){
+		el.src = "K2.png";
+	} else {}}
+
+function nextImage12(el){
+	if (el.src.match("O2.png")){
+		el.src = "O3.png";
+	} else if (el.src.match("O3.png")){
+		el.src = "O1.png";
+	} else if (el.src.match("O1.png")){
+		el.src = "O2.png";
+	} else {}}
+
+function nextImage13(el){
+	if (el.src.match("D2.png")){
+		el.src = "D3.png";
+	} else if (el.src.match("D3.png")){
+		el.src = "D1.png";
+	} else if (el.src.match("D1.png")){
+		el.src = "D2.png";
+	} else {}}
+
+function nextImage14(el){
+	if (el.src.match("H2.png")){
+		el.src = "H3.png";
+	} else if (el.src.match("H3.png")){
+		el.src = "H1.png";
+	} else if (el.src.match("H1.png")){
+		el.src = "H2.png";
+	} else {}}
+
+function nextImage15(el){
+	if (el.src.match("L2.png")){
+		el.src = "L3.png";
+	} else if (el.src.match("L3.png")){
+		el.src = "L1.png";
+	} else if (el.src.match("L1.png")){
+		el.src = "L2.png";
+	} else {}}
+
+function nextImage16(el){
+	if (el.src.match("P2.png")){
+		el.src = "P3.png";
+	} else if (el.src.match("P3.png")){
+		el.src = "P1.png";
+	} else if (el.src.match("P1.png")){
+		el.src = "P2.png";
+	} else {}}
+
+function nextImage17(el){
+	if (el.src.match("ChamberBottom1.png")){
+		el.src = "ChamberBottom2.png";
+	} else {}}
+
+function changeScreen(el){
+	var a = document.getElementById("a");
+	var b = document.getElementById("b");
+	var c = document.getElementById("c");
+	var d = document.getElementById("d");
+	var e = document.getElementById("e");
+	var f = document.getElementById("f");
+	var g = document.getElementById("g");
+	var h = document.getElementById("h");
+	var i = document.getElementById("i");
+	var j = document.getElementById("j");
+	var k = document.getElementById("k");
+	var l = document.getElementById("l");
+	var m = document.getElementById("m");
+	var n = document.getElementById("n");
+	var o = document.getElementById("o");
+	var p = document.getElementById("p");
+	if (a.src.match("A1.png") && b.src.match("B1.png") && c.src.match("C1.png") && d.src.match("D1.png") && e.src.match("E1.png") && f.src.match("F1.png") && g.src.match("G1.png") && h.src.match("H1.png") && i.src.match("I1.png") && j.src.match("J1.png") && k.src.match("K1.png") && l.src.match("L1.png") && m.src.match("M1.png") && n.src.match("N1.png") && o.src.match("O1.png") && p.src.match("P1.png")){
+	window.location = 'https://rileybrady.github.io/Phase_6/'
+	} else{}}
